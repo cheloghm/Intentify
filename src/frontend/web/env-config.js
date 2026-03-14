f2bfdb4a07b5c7b13c82f5564669f36fdf1d89a3
Graham <cheloghm@gmail.com>
2026-03-14 19:28:15 +0000

    Bug fixes

--- src/frontend/web/env-config.js
+++ src/frontend/web/env-config.js
@@ -1,2 +1,2 @@
-window.__INTENTIFY_API_BASE__ = window.__INTENTIFY_API_BASE__ || '';
-window.NEXT_PUBLIC_API_BASE_URL = window.NEXT_PUBLIC_API_BASE_URL || window.__INTENTIFY_API_BASE__;
+window.__INTENTIFY_API_BASE__ = 'http://localhost:5000';
+window.NEXT_PUBLIC_API_BASE_URL = 'http://localhost:5000';
