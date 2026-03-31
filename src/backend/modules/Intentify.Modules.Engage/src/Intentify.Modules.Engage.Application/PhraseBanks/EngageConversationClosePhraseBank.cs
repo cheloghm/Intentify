9aac27987d1dc695e44e489c0a1b6c04b6a4561e
Graham <cheloghm@gmail.com>
2026-03-31 15:32:35 +0100

    Updated close phrase bank

--- src/backend/modules/Intentify.Modules.Engage/src/Intentify.Modules.Engage.Application/PhraseBanks/EngageConversationClosePhraseBank.cs
+++ src/backend/modules/Intentify.Modules.Engage/src/Intentify.Modules.Engage.Application/PhraseBanks/EngageConversationClosePhraseBank.cs
@@ -4,6 +4,7 @@ internal static class EngageConversationClosePhraseBank
 {
     internal static readonly string[] ClosePhrases =
     [
+        // Direct endings
         "thats it",
         "that's it",
         "that is it",
@@ -12,16 +13,122 @@ internal static class EngageConversationClosePhraseBank
         "that is all",
         "nothing else",
         "nothing more",
+        "no more",
+        "no that's all",
+        "no thats all",
+        "no that’s all",
+
+        // Completion signals
         "all good",
-        "we re done",
+        "all set",
         "we're done",
+        "we re done",
         "we are done",
         "done",
-        "bye",
-        "goodbye",
+        "finished",
+        "that covers it",
+        "that helps",
+        "that works",
+        "that answers it",
+
+        // Gratitude + closure
         "thanks thats all",
         "thanks that's all",
         "thank you thats all",
-        "thank you that's all"
+        "thank you that's all",
+        "thanks that's everything",
+        "thank you that's everything",
+        "thanks that's it",
+        "thank you that's it",
+        "thanks for your help",
+        "thank you for your help",
+        "appreciate it",
+        "much appreciated",
+        "thanks",
+        "thank you",
+        "cheers",
+        "thanks a lot",
+        "thank you very much",
+
+        // Soft polite exits
+        "okay thanks",
+        "ok thanks",
+        "alright thanks",
+        "great thanks",
+        "perfect thanks",
+        "sounds good thanks",
+        "that’s fine thanks",
+        "that's fine thanks",
+
+        // Conversation wrap-ups
+        "i'm good",
+        "im good",
+        "i’m good",
+        "i'm all set",
+        "im all set",
+        "i’m all set",
+        "i'm sorted",
+        "all sorted",
+        "that's sorted",
+        "we're sorted",
+        "we are sorted",
+
+        // Goodbye variations
+        "bye",
+        "goodbye",
+        "bye bye",
+        "see you",
+        "see you later",
+        "see ya",
+        "talk later",
+        "catch you later",
+        "take care",
+        "have a good day",
+        "have a great day",
+        "have a nice day",
+
+        // Casual / informal
+        "cool thanks",
+        "nice thanks",
+        "ok cool",
+        "alright cool",
+        "all good thanks",
+        "sweet thanks",
+        "nice one",
+        "nice one thanks",
+
+        // Confirmation of completion
+        "that should do",
+        "that will do",
+        "this is enough",
+        "this works",
+        "this is fine",
+        "that’s enough",
+        "that's enough",
+
+        // Business / professional closure
+        "that answers my question",
+        "that resolves it",
+        "issue resolved",
+        "problem solved",
+        "no further questions",
+        "no further queries",
+        "no further assistance needed",
+
+        // Multi-step flow completion (booking/order/etc.)
+        "order complete",
+        "booking complete",
+        "appointment confirmed",
+        "that's booked",
+        "that's confirmed",
+        "all booked",
+        "all confirmed",
+
+        // Emoji / modern usage
+        "thanks 👍",
+        "thank you 👍",
+        "ok 👍",
+        "all good 👍",
+        "cheers 👍"
     ];
 }
